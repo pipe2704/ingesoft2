05d2bdfc46e3d170b8269540a5196aa9b27b274f
pipe2704 <88251120+pipe2704@users.noreply.github.com>
2024-03-01 15:48:00 -0500

    ejercicio operadores aritmeticos

---
+++ entrypoint.py
@@ -0,0 +1,13 @@
+# Solicitamos dos números enteros del usuario
+num1 = int(input())
+num2 = int(input())
+
+# Calculamos la suma, la diferencia y el producto de los dos números
+sum = num1 + num2
+diff = num1 - num2
+product = num1 * num2
+
+# Imprimimos los resultados en tres líneas separadas
+print(sum)      # Suma
+print(diff)     # Diferencia
+print(product)  # Producto
